251e02305cc52501cdee6c7104b7cb2d478c57ed
weizai133 <31020832+weizai133@users.noreply.github.com>
2018-10-11 11:25:06 +1300

    Add Components folder and header.js

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import header from "./components/header";
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <header />
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
---
+++ components/header.js
@@ -0,0 +1,10 @@
+import React from "React";
+import {Text} from "react-native";
+
+const header=()=>{
+    return (
+        <Text>Albums</Text>
+    );
+}
+
+export default header;
